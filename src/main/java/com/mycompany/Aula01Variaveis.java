67f88093d29fb7afb024153023eb3dbd110ee1e5
vitor-portela-lopes <vitor.portela.lopes@escola.pr.gov.br>
2025-03-26 13:51:26 +0000

    aula 01 de variaveis

---
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -0,0 +1,14 @@
+package com.mycompany;
+
+public class Aula01Variaveis {
+    
+    public static void main(String[]  args){
+        //variavel lógica
+       
+        //tipo primitivo
+        boolean ValorLogicoPrimitivo = true;
+
+        //tipo abstrato
+        boolean ValorLogicoAbstrato = false;
+    } 
+}
