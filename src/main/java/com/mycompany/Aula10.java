70d0fb7acb798de804be86b0144f1cd92602ad76
vitor-portela-lopes <vitor.portela.lopes@escola.pr.gov.br>
2025-08-06 19:27:17 +0000

    aula10 Modificadores de acesso, parte 2

---
+++ src/main/java/com/mycompany/Aula10.java
@@ -0,0 +1,27 @@
+package com.mycompany.poo.aula10;
+
+public class Pessoa {
+    /*
+     * Modificadores de Acesso
+     * 
+     * Quando trabalhamos com o conceito de POO, utilizamos modificadores de acesso,
+     * para dar segurança para nossas classes.
+     * 
+     * Utilizamos elas como prefixo para:
+     * -Classes
+     * -Interfaces
+     * -Atributos
+     * -Métodos
+     */
+    public String nome; // Público, é visivel em qualquer classe.
+    protected int idade; // Protegido, é visível somente em classes filhas (Herança).
+    private double salario; // Privado, é visível somente dentro da própria classe.
+    double altura; //Friendly ou Público, apesar de não estar explicitado, ele também é visivel.
+
+    public void exibirDados() {
+        System.out.println("--- Dados da pessoa ---");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura (m): " + altura);
+    }
+}
--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,44 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+package com.mycompany.poo.aula10;
 
-package com.mycompany.app;
+import com.mycompany.Pessoa;
 
 public class App {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("hello 3ds");
+        Pessoa pessoa = new Pessoa();
+        pessoa.nome = "Dani";
+        pessoa.idade = 17;
+        // pessoa.salario = 1.000; atributos privados não são visiveis
+
+        Pessoa pessoa1 = new Pessoa();
+        Pessoa pessoa2 = new Pessoa();
+
+        pessoa1.nome = "Renan";
+        pessoa1.idade = 20;
+        pessoa1.altura = 1.60;
+
+        pessoa2.nome = "Jean";
+        pessoa2.idade = 26;
+        pessoa2.altura = 1.70;
+
+        pessoa1.exibirDados();
+        pessoa2.exibirDados();
+
+        pessoa1.atribuirSalario(2000.0, 500);
+        pessoa2.atribuirSalario(3500.0, );
+
+        /*System.out.println(" --- Dados da pessoa --- ");
+        System.out.println("Nome: " + pessoa1.nome);
+        System.out.println("Idade: " + pessoa1.idade);
+        System.out.println("Altura (m):" + pessoa1.altura);
+
+        System.out.println("--- Dados da pessoa ---");
+        System.out.println("Nome :  " + pessoa2.nome);
+        System.out.println("Idade: " + pessoa2.idade);
+        System.out.println("Altura (m):" + pessoa2.altura);*/
+
     }
-}
+
+}    
